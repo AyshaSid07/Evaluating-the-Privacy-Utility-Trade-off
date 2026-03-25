b06ba2f489ee4e30d1e275d8d62af6ca7e456479
Hampus Lyrstrand <hampus.lyrstrand@ericsson.com>
2026-03-25 15:32:16 +0100

    changes

---
+++ datasets/datasets_code/preprocess_mendeley.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import ipaddress
+
+df = pd.read_csv("../mendeley_dataset.csv")
+
+cols_to_check = [
+    'City', 'Region', 'Country', 'Postal Code', 'Latitude', 
+    'Longitude', 'Timezone', 'ISP', 'Organization', 'Autonomous System'
+]
+
+df.replace('N/A', np.nan, inplace=True)
+
+df = df.dropna(subset=cols_to_check, how='all')
+
+df.replace('TELEF�NICA BRASIL S.A', 'TELEFONICA BRASIL S.A', inplace=True)
+df.to_csv("../mendeley_dataset2.csv", index=False)
