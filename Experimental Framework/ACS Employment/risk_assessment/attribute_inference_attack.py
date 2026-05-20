71489ded52ad0d719b03f1a2107a9e3d837a735d
Hampus Lyrstrand <hampus@lyrstrand.se>
2026-05-20 16:00:30 +0200

    forgot to add ACS Employment

--- Experimental Framework/ACS Employment/risk_assessment/attribute_inference_attack.py
+++ Experimental Framework/ACS Employment/risk_assessment/attribute_inference_attack.py
@@ -104,6 +104,7 @@ datasets_to_test = {
     "ARX Employment, k = 5, t = 0.15": pd.read_csv('../datasets/ARX_acs_employment_k5_t0.15.csv'),
     "DP Employment, epsilon = 10.0": pd.read_csv('../datasets/DP_employment_epsilon_10_0.csv'),
     "DP Employment, epsilon = 5.0":  pd.read_csv('../datasets/DP_employment_epsilon_5_0.csv'),
+    "DP Employment, epsilon = 3.0":  pd.read_csv('../datasets/DP_employment_epsilon_3_0.csv'),
     "DP Employment, epsilon = 1.0":  pd.read_csv('../datasets/DP_employment_epsilon_1_0.csv'),
     "DP Employment, epsilon = 0.5":  pd.read_csv('../datasets/DP_employment_epsilon_0_5.csv'),
     "DP Employment, epsilon = 0.1":  pd.read_csv('../datasets/DP_employment_epsilon_0_1.csv'),
